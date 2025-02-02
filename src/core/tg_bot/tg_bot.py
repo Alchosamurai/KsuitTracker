2dd6a215b068c37a3d33f20596b394ba093d431e
Alchosamurai <alchosan.py@gmail.com>
2025-02-02 12:00:52 +0600

    добавил функционал настроек

--- src/core/tg_bot/tg_bot.py
+++ src/core/tg_bot/tg_bot.py
@@ -6,7 +6,7 @@ from src.database.models.task import Task
 from src.core.utils import time_options, shedule 
 from src.core.utils.shedule import start_scheduler
 import re
-from telebot.types import ReplyKeyboardMarkup, KeyboardButton
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 from datetime import datetime, timedelta
 import logging  
 # Настройка логирования
@@ -21,10 +21,11 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)  
 
 bot = telebot.TeleBot(config.TG_BOT_TOKEN, parse_mode=None)
+user_states = {}
 time_pattern = re.compile(r'^([0-1]?[0-9]|2[0-3])[: ]([0-5][0-9])$')
 
 
-
+#* START *
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     chat_id = message.chat.id
@@ -37,19 +38,7 @@ def send_welcome(message):
 2) Бот сам рассчитает, сколько еще часов за сегодня осталось закрыть, чтобы не объебаться в конце месяца
 """, reply_markup=create_keyboard(),parse_mode='Markdown')
 
-@bot.message_handler(func=lambda message: message.text == "Статистика за сегодня 📊")
-def send_today_stats(message):
-    logger.info(f"User {message.from_user.username} (chat_id: {message.chat.id}) requested today's statistics.")  
-    response = ""
-    tasks = Task.today_user_stats(message.chat.id)
-    total_time = timedelta(seconds=0)
-    for task in tasks:
-        response += f"{task.hour:02}:{task.min:02} в {task.datetime.hour:02}:{task.datetime.minute:02}\n"
-        total_time += timedelta(hours=task.hour, minutes=task.min)
-    response += f"*Общее время*: {time_options.timedelta_to_hhmm(total_time)}"
-    bot.reply_to(message, response, parse_mode='Markdown', reply_markup=create_keyboard())
-
-
+#* STATS *
 @bot.message_handler(func=lambda message: time_pattern.match(message.text))
 def handle_time(message):
     time = message.text.replace(' ', ':')
@@ -57,12 +46,73 @@ def handle_time(message):
     actual_time = datetime.now()
     cur_time = Task.get_current_time_by_date(chat_id=message.chat.id, target_date=actual_time)
     cur_time = time_options.timedelta_to_hhmm(cur_time)
-    tracker = Tracker(current_time=cur_time)
+    user = User.get(message.chat.id)
+    daily_target = f"{user.daily_target_hour:02}:{user.daily_target_min:02}"
+    tracker = Tracker(current_time=cur_time, daily_target=daily_target)
     cur_time = tracker.uppload_work_time(time)
     Task.create(message.chat.id, int(time.split(':')[0]), int(time.split(':')[1]))
     logger.info(f"Task created for user {message.from_user.username} (chat_id: {message.chat.id}) with time {time}.")  
     bot.reply_to(message, f"Осталось {cur_time} часов", reply_markup=create_keyboard())
 
+@bot.message_handler(func=lambda message: message.text == "Статистика за сегодня 📊")
+def send_today_stats(message):
+    logger.info(f"User {message.from_user.username} (chat_id: {message.chat.id}) requested today's statistics.")  
+    user = User.get(message.chat.id)
+    user_target_timedelta = timedelta(hours=user.daily_target_hour, minutes=user.daily_target_min)
+    response = ""
+    response += f"*Цель за сегодня*: {user.daily_target_hour:02}:{user.daily_target_min:02}\n"
+    tasks = Task.today_user_stats(message.chat.id)
+    total_time = timedelta(seconds=0)
+    for task in tasks:
+        response += f"{task.hour:02}:{task.min:02} в {task.datetime.hour:02}:{task.datetime.minute:02}\n"
+        total_time += timedelta(hours=task.hour, minutes=task.min)
+    response += f"*Общее время*: {time_options.timedelta_to_hhmm(total_time)}"
+    response += f"\n*Осталось*: {time_options.timedelta_to_hhmm(user_target_timedelta - total_time)}"
+    bot.reply_to(message, response, parse_mode='Markdown', reply_markup=create_keyboard())
+
+#* SETTINGS *
+@bot.message_handler(func=lambda message: message.text == "Настройки ⚙️")
+def set_settings(message):
+    logger.info(f"User {message.from_user.username} (chat_id: {message.chat.id}) requested settings.")
+    user = User.get(message.chat.id)
+    response = f"""
+Текущая дневная цель: {user.daily_target_hour:02}:{user.daily_target_min:02}
+Текущая месячная цель: {user.monthly_target_hour:02} часов
+Хотите изменить параметры? 
+"""
+    bot.reply_to(message, response, reply_markup=create_settings_keyboard())
+
+@bot.callback_query_handler(func=lambda call: True) # Ловим callback
+def callback_query(call):
+    if call.data == "yes":
+        user_states[call.message.chat.id] = "setting"
+        bot.answer_callback_query(call.id, "Отлично! Начинаем настройку")
+        bot.send_message(call.message.chat.id, """Напишите дневную цель в формате ЧЧ:ММ или ЧЧ MM,
+после чего оставьте пробел и введите месячную цель в часах.
+*Например*:
+08:00 160 ✅
+07 00 140 ✅
+07:00:140 ✅ 
+""", parse_mode='Markdown')
+        # Здесь можно добавить логику для изменения параметров
+    elif call.data == "no":
+        bot.answer_callback_query(call.id, "Настройка отменена")
+        bot.send_message(call.message.chat.id, "На нет и суда нет)")
+    else:
+        bot.answer_callback_query(call.id, "🤡")
+
+
+@bot.message_handler(func=lambda message: user_states[message.chat.id] == "setting")
+def set_settings(message):
+    user = User.get(message.chat.id)
+    text = message.text.replace(' ', ':')
+    text = text.split(':')
+    if user:
+        user = User.update(message.chat.id, daily_target_hour=int(text[0]), daily_target_min=int(text[1]), monthly_target_hour=int(text[2]))
+        bot.send_message(message.chat.id, "Настройки сохранены!")
+
+
+#* Functions *
 def send_message_to_chat(chat_id: int, text: str):
     """
     Отправляет сообщение в указанный чат.
@@ -86,16 +136,25 @@ def echo_all(message):
         logger.warning(f"User not found for chat_id: {message.chat.id}.") 
         bot.reply_to(message, "Пользователь не найден.")
 
+#* Buttons *
 
 def create_keyboard():
     """
     Создает Reply-клавиатуру с одной кнопкой.
     """
     keyboard = ReplyKeyboardMarkup(resize_keyboard=True)  # Автоматическое изменение размера клавиатуры
-    button = KeyboardButton("Статистика за сегодня 📊")
+    today_stat_button = KeyboardButton("Статистика за сегодня 📊")
+    settings_button = KeyboardButton("Настройки ⚙️") #🛠
     #button2 = KeyboardButton("Статистика за весь месяц 📊📊📊")  # Текст на кнопке
-    keyboard.add(button)
+    keyboard.add(today_stat_button)
+    keyboard.add(settings_button)
     #keyboard.add(button2)  # Добавляем кнопку в клавиатуру
+    return keyboard
+def create_settings_keyboard():
+    keyboard = InlineKeyboardMarkup()
+    yes_button = InlineKeyboardButton("Да ✅", callback_data="yes")
+    no_button = InlineKeyboardButton("Нет ❌", callback_data="no")
+    keyboard.add(yes_button, no_button)
     return keyboard
 
 try:
--- src/database/models/user.py
+++ src/database/models/user.py
@@ -8,6 +8,9 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(50))
     chat_id = Column(Integer, unique=True)
+    daily_target_hour = Column(Integer, default=8)
+    daily_target_min = Column(Integer, default=0)
+    monthly_target_hour = Column(Integer, default=160)
 
     @classmethod
     def create(cls, name: str, chat_id: int):
@@ -24,7 +27,7 @@ class User(Base):
                 return session.query(cls).filter(cls.chat_id == chat_id).first()
             
             # Создаем нового пользователя
-            user = cls(name=name, chat_id=chat_id)
+            user = cls(name=name, chat_id=chat_id, daily_target_hour=8, daily_target_min=0, monthly_target_hour=160)
             session.add(user)
             session.commit()
             session.refresh(user)
@@ -48,6 +51,29 @@ class User(Base):
         """
         with Session(engine) as session:
             return session.query(cls).all()
+    
+    @classmethod
+    def update(cls, chat_id: int, **kwargs):
+        """
+        Обновляет поля пользователя по chat_id.
+        :param chat_id: Уникальный идентификатор чата
+        :param kwargs: Поля и их новые значения (например, name="Новое имя")
+        :return: Обновленный объект пользователя или None, если пользователь не найден
+        """
+        with Session(engine) as session:
+            # Находим пользователя по chat_id
+            user = session.query(cls).filter(cls.chat_id == chat_id).first()
+            if not user:
+                return None  # Пользователь не найден
+            
+            # Обновляем поля
+            for key, value in kwargs.items():
+                if hasattr(user, key):  # Проверяем, существует ли поле
+                    setattr(user, key, value)
+            
+            session.commit()  # Сохраняем изменения
+            session.refresh(user)  # Обновляем объект
+            return user
 
     @classmethod
     def create_table(cls):
